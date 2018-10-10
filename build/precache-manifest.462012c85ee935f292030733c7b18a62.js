537a6f4d79f25e88e64e47b0526556ef267ed906
Manuel Dugué <manuel.dugue@comvel.de>
2018-10-10 09:28:23 +0200

    added .env and build

---
+++ build/precache-manifest.462012c85ee935f292030733c7b18a62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "73a26fdf18a087485456",
+    "url": "/static/js/main.73a26fdf.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "73a26fdf18a087485456",
+    "url": "/static/css/main.1b48ca3f.chunk.css"
+  },
+  {
+    "revision": "50638d42635ec47c77cf64a0f0de6465",
+    "url": "/index.html"
+  }
+];
